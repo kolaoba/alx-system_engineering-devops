9581fa3ecbf2647952306659ed64c58104ef3465
kolaoba <kolaobajuluwa@gmail.com>
2023-01-16 20:08:42 +0100

    modified task 3

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,22 +4,25 @@ return information about his/her TODO list progress
 and exports data to json
 """
 
+import json
 import requests
 from sys import argv
-import json
 
+if __name__ == '__main__':
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
 
-BASE_URL = 'https://jsonplaceholder.typicode.com'
+    user = requests.get('{}/users/{}'.format(BASE_URL, argv[1])).json()
+    todos = requests.get(
+        '{}/todos'.format(BASE_URL),
+        params={
+            "userId": argv[1]}).json()
 
-user = requests.get(f'{BASE_URL}/users/{argv[1]}').json()
-todos = requests.get(f'{BASE_URL}/todos', params={"userId": argv[1]}).json()
-
-with open(f'{argv[1]}.json', 'w', encoding='utf8') as f:
-    tasks = []
-    for todo in todos:
-        tasks.append({"task": f'{todo["title"]}',
-                      "completed": f'{str(todo["completed"])}',
-                      "username": f'{user["name"]}'})
-    data = {f"{argv[1]}": tasks}	
-    json_string = json.dumps(data)
-    f.write(json_string.rstrip())
+    with open('{}.json'.format(argv[1]), 'w', encoding='utf8') as f:
+        tasks = []
+        for todo in todos:
+            tasks.append({"task": '{}'.format(todo.get("title")),
+                          "completed": '{}'.format(str(todo.get("completed"))),
+                          "username": '{}'.format(user.get("username"))})
+        data = {"{}".format(argv[1]): tasks}
+        json_string = json.dumps(data)
+        f.write(json_string.rstrip())
