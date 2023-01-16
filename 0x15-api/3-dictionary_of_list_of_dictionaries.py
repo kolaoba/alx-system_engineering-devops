b7c934b0f3821eb46a327597577fd42727cc9799
kolaoba <kolaobajuluwa@gmail.com>
2023-01-16 20:49:50 +0100

    added task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Exports all todo data to json
+"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+    users = requests.get('{}/users'.format(BASE_URL)).json()
+    todos = requests.get(
+        '{}/todos'.format(BASE_URL)).json()
+
+    with open('todo_all_employees.json', 'w', encoding='utf8') as f:
+        output = {}
+        for user in users:
+            tasks = []
+            for todo in todos:
+                tasks.append({"username": user.get("username"),
+                              "task": todo.get("title"),
+                              "completed": todo.get("completed")})
+            output[user.get("id")] = tasks
+        json.dump(output, f)
