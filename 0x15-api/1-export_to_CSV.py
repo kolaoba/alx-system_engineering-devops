b5b7a919eaae144031e3f59f95b6c30fb730d5ea
kolaoba <kolaobajuluwa@gmail.com>
2023-01-16 19:52:19 +0100

    modified task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,13 +10,13 @@ from sys import argv
 
 BASE_URL = 'https://jsonplaceholder.typicode.com'
 
-user = requests.get('{}/users/{}'.format(BASE_URL, argv[1])).json()
-todos = requests.get('{}/todos'.format(BASE_URL),
-                     params={"userId": argv[1]}).json()
+user = requests.get("{}/users/{}".format(BASE_URL, argv[1])).json()
+todos = requests.get(
+    "{}/todos".format(BASE_URL), params={"userId": argv[1]}).json()
 
-with open('{}.csv'.format(argv[1]), 'w', encoding='utf8') as f:
+with open("{}.csv".format(argv[1]), 'w', encoding='utf8') as f:
     writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
     for todo in todos:
-        writer.writerow(['{}'.format(argv[1]), '{}'.format(user.get("name")),
-                         '{}'.format(str(todo.get("completed"))),
-                         '{}'.format(todo.get("title"))])
+        writer.writerow(["{}".format(argv[1]), "{}".format(user.get("name")),
+                         "{}".format(str(todo.get("completed"))),
+                         "{}".format(todo.get("title"))])
